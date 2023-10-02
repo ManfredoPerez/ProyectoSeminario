dd34415e7b878d98fadecf7580682cb73c802de0
perezmanfredo959@gmail.com <perezmanfredo959@gmail.com>
2023-10-02 17:42:47 -0600

    Primer Commit Login

---
+++ src/scenas/login/index.jsx
@@ -0,0 +1,34 @@
+
+import xelaImage from './logoMuni.png'; 
+import "./style.css"
+
+const Login = () =>{
+    return(
+        <div className="login template d-flex justify-content-center align-items-center vh-100 ">
+            <div className="form_container p-5 rounded bg-white">
+                <form action="">
+                    <div className="text-center">
+                        <img src={xelaImage} alt="Logo" className="mb-4" style={{ width: '50%', height: 'auto' }} />
+                    </div>
+                    <h3 className="text-center">Login</h3>
+                    <div className="mb-4">
+                        <label htmlFor="user"> <h4>Usuario</h4> </label>
+                        <input type="text" placeholder="Ingrese su Usuario" className="form-control" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="contraseña"> <h4>Contraseña</h4> </label>
+                        <input type="contraseña" placeholder="Ingrese su contraseña" className="form-control" />
+                    </div>
+                    
+                    
+                    <div className="d-grid mb-4">
+                        <button className="btn btn-primary">Iniciar sesion</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
