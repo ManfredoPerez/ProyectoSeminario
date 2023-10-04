25c9df68a39a0213bd70f340988a4ae1de0ebc21
EdvinCanastuj <ecanastujv@miumg.edu.gt>
2023-10-04 11:31:08 -0600

    configuración env

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import { config } from "dotenv";
+config();
+export default {
+    host: process.env.HOST || "",
+    port:process.env.PORT || "",
+    database: process.env.DATABASE || "",
+    user: process.env.USER || "",
+    password: process.env.PASSWORD || ""
+};
